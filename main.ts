3c968b3a0f294897442326757a0bfbbf2df888fe
Chill Papa <136734912+ChillPapa@users.noreply.github.com>
2023-07-02 10:28:33 +0000

    idk

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
 import { createRouter, serve } from "./mod.ts";
-import * as mod from "https://deno.land/std@0.192.0/streams/mod.ts";
+import {
+  readAll,
+  readerFromStreamReader,
+} from "https://deno.land/std@0.192.0/streams/mod.ts";
+
+const echo = async (req: Request): Promise<string> => {
+  if (req.body) {
+    const decoder = new TextDecoder();
+    const buf = await readAll(
+      readerFromStreamReader(new ReadableStreamDefaultReader(req.body)),
+    );
+    if (decoder.decode(buf)) {
+      return decoder.decode();
+    }
+    return "body is empty";
+  }
+  return "no body provided";
+};
 
 const router = createRouter()
   .get(/\//, (_) => "hello world")
-  .post(/\//, async (req) => {
-    if (req.body) {
-      const decoder = new TextDecoder();
-      const buf = await mod.readAll(
-        mod.readerFromStreamReader(new ReadableStreamDefaultReader(req.body)),
-      );
-      return decoder.decode(buf);
-    }
-    return "{}";
-  });
+  .post(/\//, echo);
 
 await serve(8000, router);
